85bf4cf3a881ad050ae75f833cf9fc98d92980b7
HolyPygovka <44292627+HolyPygovka@users.noreply.github.com>
2020-05-30 15:01:15 +0300

    Add files via upload

---
+++ slider_project3/js/script.js
@@ -0,0 +1,10 @@
+let mainSlider = document.querySelector('.main_slider');
+mainSlider.lightSlider({
+    slides: 1,
+    slidesWidth: 300,
+    slidesHeight: 200,
+    nav: true,
+    autoplay: false,
+    autoplaySpeed: 500,
+    loop: true,
+});
